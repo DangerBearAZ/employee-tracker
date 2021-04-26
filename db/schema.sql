6119219c439d86cd7444bab2d54cff38996b2a66
claire ogesen <claire.ogesen.co@gmail.com>
2021-04-25 21:52:32 -0700

    clean

--- db/schema.sql
+++ db/schema.sql
@@ -11,11 +11,19 @@ USE employee_tracker;
 
 DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
+
+ CREATE TABLE department (
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     department_name VARCHAR(30) NOT NULL
+ );
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(7,2) NOT NULL
+    salary DECIMAL(7,2) NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee ( 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,22 @@
 
 
-INSERT INTO roles (title, salary)
+
+INSERT INTO department (department_name)
+VALUES 
+("Naps"),
+("treats"),
+("quality"),
+("distruction");
+
+INSERT INTO roles (title, salary, department_id)
 VALUES
-('Boss', 65159.69),
-('minion', 12.12),
-('slave', 0.01); 
+('Boss', 65159.69, 1),
+('minion', 12.12, 2),
+('slave', 0.01, 3); 
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES 
 ('Odin', 'Dog', 1),
 ('Apollo', 'Cat', 1),
 ('Suzuki', 'Motorcycle', 1);
+
